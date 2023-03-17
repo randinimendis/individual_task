4ce7cb8c5f532db5fd045b6606ae5cfcd9f002ea
Randini Mendis <99355199+randinimendis@users.noreply.github.com>
2023-03-17 09:38:14 +0530

    initial commit

---
+++ app/src/main/java/com/example/mad_pro/therd_item_by.kt
@@ -0,0 +1,11 @@
+package com.example.mad_pro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class therd_item_by : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_therd_item_by)
+    }
+}
